c607a6f62f1feeb08defaaa160d1ee0cd16480fa
Miconen <micropizza.mc@gmail.com>
2022-08-14 23:23:11 +0300

    Updated xrandr script

---
+++ arch/xrandr.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-0 --off --output DP-0 --mode 1920x1080 --rate 119.98 --pos 4480x180 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 2560x1440 --rate 165.08 --pos 1920x0 --rotate normal --output DP-3 --off --output DP-4 --mode 1920x1080 --rate 119.98 --pos 0x180 --rotate normal --output DP-5 --off
