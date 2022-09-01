5c78e2a1737cf510afa72bc765fd364064f9f420
Miconen <micropizza.mc@gmail.com>
2022-09-01 21:11:14 +0300

    Changes to init.lua for nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,9 +5,9 @@ require('coc-config')
 require('lualine').setup()
 
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = "all",
-    ignore_install = { "phpdoc" },
-    context_commentstring = {
+        ensure_installed = "all",
+        ignore_install = { "phpdoc" },
+        context_commentstring = {
         enable = true
     },
     highlight = {
@@ -21,10 +21,14 @@ require'nvim-treesitter.configs'.setup {
 
 return require('packer').startup(function()
     use 'wbthomason/packer.nvim'
-    use {'neoclide/coc.nvim', branch = 'release'}
-    use { "ellisonleao/gruvbox.nvim" }
-    use { "folke/tokyonight.nvim" }
+    use {
+        'neoclide/coc.nvim',
+        branch = 'release'
+    }
+    use 'ellisonleao/gruvbox.nvim'
+    use 'folke/tokyonight.nvim'
     use 'nvim-treesitter/nvim-treesitter'
+    use 'nvim-treesitter/nvim-treesitter-context'
     use 'tpope/vim-commentary'
     use 'JoosepAlviste/nvim-ts-context-commentstring'
     use 'lukas-reineke/indent-blankline.nvim'
@@ -32,15 +36,17 @@ return require('packer').startup(function()
         'nvim-telescope/telescope.nvim',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
-    use { "nvim-telescope/telescope-file-browser.nvim" }
+    use "nvim-telescope/telescope-file-browser.nvim"
     use {
         "ur4ltz/surround.nvim",
         config = function()
         require"surround".setup {mappings_style = "surround"}
         end
     }
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+    use 'wikitopian/hardmode'
+    use 'ionide/Ionide-vim'
 end)
