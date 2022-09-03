0955ffd83f060fcab0feb760742b2c9eb33c86eb
Miconen <micropizza.mc@gmail.com>
2022-09-03 12:40:29 +0300

    Seperated global configuration from zshrc.sh

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -1,26 +1,4 @@
-# Global settings file
-# Plugins
-source ~/Repos/zsh-autocomplete/zsh-autocomplete.plugin.zsh
-
-# Zoxide
-eval "$(zoxide init zsh)"
-
-# Prompt
-autoload -Uz vcs_info
-precmd() { vcs_info }
-zstyle ':vcs_info:git:*' formats '%b '
-setopt PROMPT_SUBST
-PROMPT='%F{white}%*%f %F{blue}%~%f %F{red}${vcs_info_msg_0_}%f$ '
-
-# Aliases
-# Configs
-alias .zshrc="$EDITOR ~/.config/.mico/zsh/zshrc.sh"
-alias .vimrc="$EDITOR ~/.config/nvim/init.lua"
-alias .coc="$EDITOR ~/.config/nvim/coc-settings.json"
-
-# Utility
-alias :q=exit
-alias c="clear"
+source ~/.config/.mico/zsh/global.sh
 
 case `uname` in
     # Linux settings file
