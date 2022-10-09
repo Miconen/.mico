9cb9a4681723e436ba284f70f3d02381f441135c
Mico <9155442+Miconen@users.noreply.github.com>
2022-10-09 10:31:49 +0300

    Update colors.lua

--- nvim/lua/colors.lua
+++ nvim/lua/colors.lua
@@ -1,10 +1,11 @@
--- Gruvbox Dark
--- vim.o.background = "dark"
--- vim.cmd([[colorscheme gruvbox]])
-
 -- Tokyonight, transparent
+require("tokyonight").setup {
+  style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  transparent = true, -- Enable this to disable setting the background color
+  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+}
 vim.o.termguicolors = true
-vim.g.tokyonight_transparent = true
 vim.g.tokyonight_lualine_bold = true
 vim.cmd([[ colorscheme tokyonight ]])
 vim.cmd([[ silent! highlight Normal guibg=none ]])
