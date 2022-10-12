091f88e7c737c3ccb1c532e46ba417bfc4cf66c0
Miconen <micropizza.mc@gmail.com>
2022-10-12 16:55:25 +0300

    Cleaned up init.lua for a more redable/presentable experience

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,8 @@
+-- Basic vim setup
 require('basics')
+-- Colors, theme and styling
 require('colors')
+-- Plugin specific config files
 require('telescope-config')
 require('coc-config')
 require('gitsigns-config')
@@ -7,33 +10,5 @@ require('scrollbar-config')
 require('treesitter-config')
 require('lualine').setup()
 
-return require('packer').startup(function()
-    use 'wbthomason/packer.nvim'
-    use {
-        'neoclide/coc.nvim',
-        branch = 'release'
-    }
-    use 'folke/tokyonight.nvim'
-    use 'nvim-treesitter/nvim-treesitter'
-    use 'nvim-treesitter/nvim-treesitter-context'
-    use 'tpope/vim-commentary'
-    use 'JoosepAlviste/nvim-ts-context-commentstring'
-    use 'lukas-reineke/indent-blankline.nvim'
-    use 'lewis6991/gitsigns.nvim'
-    use 'petertriho/nvim-scrollbar'
-    use {
-        'nvim-telescope/telescope.nvim',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
-    use "nvim-telescope/telescope-file-browser.nvim"
-    use {
-        "ur4ltz/surround.nvim",
-        config = function()
-        require"surround".setup {mappings_style = "surround"}
-        end
-    }
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
-end)
+-- Imports of vim plugins
+return require('plugins')
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,30 @@
+require('packer').startup(function()
+    use 'wbthomason/packer.nvim'
+    use {
+        'neoclide/coc.nvim',
+        branch = 'release'
+    }
+    use 'folke/tokyonight.nvim'
+    use 'nvim-treesitter/nvim-treesitter'
+    use 'nvim-treesitter/nvim-treesitter-context'
+    use 'tpope/vim-commentary'
+    use 'JoosepAlviste/nvim-ts-context-commentstring'
+    use 'lukas-reineke/indent-blankline.nvim'
+    use 'lewis6991/gitsigns.nvim'
+    use 'petertriho/nvim-scrollbar'
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use "nvim-telescope/telescope-file-browser.nvim"
+    use {
+        "ur4ltz/surround.nvim",
+        config = function()
+        require"surround".setup {mappings_style = "surround"}
+        end
+    }
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+end)
