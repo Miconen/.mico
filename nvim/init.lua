9d75dba851a1667bdc024f4a824e8296510844a1
Miconen <micropizza.mc@gmail.com>
2022-10-12 09:45:32 +0300

    Extract configs to seperate files and add scrollbar config

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,22 +2,9 @@ require('basics')
 require('colors')
 require('telescope-config')
 require('coc-config')
+require('scrollbar-config')
+require('treesitter-config')
 require('lualine').setup()
-
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = "all",
-    ignore_install = { "phpdoc" },
-    context_commentstring = {
-        enable = true
-    },
-    highlight = {
-        enable = true,
-        disable = { "lua" }
-    },
-    indent = {
-        enable = true
-    }
-}
 
 return require('packer').startup(function()
     use 'wbthomason/packer.nvim'
