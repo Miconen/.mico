d968414cd1d1cdd9e72eb2ca77c9246fdf90bfcc
Miconen <micropizza.mc@gmail.com>
2023-02-26 13:03:35 +0200

    Remove dependencies.lua

--- nvim/lua/mico/dependencies.lua
+++
@@ -1,7 +0,0 @@
-vim.notify = require("notify")
-
-if vim.fn.executable('rg') ~= 1 then
-    vim.notify("Couldn't find the command: rg (ripgrep)\nSome Telescope functionality might not work.", "warn", {
-        title = "Missing dependency",
-    })
-end
--- nvim/lua/mico/init.lua
+++ nvim/lua/mico/init.lua
@@ -1,9 +1,6 @@
 -- Basic vim setup
 require('mico.settings')
 
--- Check and warn about missing dependencies
-require('mico.dependencies')
-
 require('lualine').setup()
 
 -- Imports of vim plugins
