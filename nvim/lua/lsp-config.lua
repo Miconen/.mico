e120ef40ff67aae0c3ac8af62c120613e2ed35f1
Miconen <micropizza.mc@gmail.com>
2023-02-26 02:42:44 +0200

    Seperate lsp handling to an array

--- nvim/lua/lsp-config.lua
+++ nvim/lua/lsp-config.lua
@@ -1,30 +1,49 @@
-require("mason").setup()
-require("mason-lspconfig").setup {
-    ensure_installed = { "lua_ls" },
+-- Array of lsps to initialize
+local lsps = {
+    -- Lua
+    [1] = "lua_ls",
+    -- C#
+    [2] = "omnisharp",
+    -- TypeScript
+    [3] = "tsserver",
+    [4] = "angularls",
+    [5] = "astro",
+    -- HTML & CSS
+    [6] = "html",
+    [7] = "cssls",
+    [8] = "emmet_ls",
 }
 
+-- Mason setup
+require("mason").setup()
+require("mason-lspconfig").setup {
+    ensure_installed = lsps
+}
+
+-- Keybinds
 local on_attach = function(_, _)
     -- utility
     vim.keymap.set("n", "<leader>.", vim.lsp.buf.code_action, {})
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {silent = true})
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {silent = true})
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { silent = true })
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { silent = true })
     vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, {silent = true, noremap = true})
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, { silent = true, noremap = true })
 
     -- errors
-    vim.keymap.set("n", "<leader>l", function() vim.diagnostic.automatic() end, {})
     vim.keymap.set("n", "ge", function() vim.diagnostic.goto_next() end, {})
     vim.keymap.set("n", "gE", function() vim.diagnostic.goto_prev() end, {})
+
+    -- format
+    vim.keymap.set("n", "<leader>l", function() vim.lsp.buf.format() end, {})
 end
 
+-- Autocompletion
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-require("lspconfig").lua_ls.setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-}
-
-require("lspconfig").omnisharp.setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-}
+-- Initialize language servers from lsps array
+for _, lsp in ipairs(lsps) do
+    require("lspconfig")[lsp].setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    }
+end
