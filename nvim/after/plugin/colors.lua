519522d1f22f12774ee7c7e8656a87c484f0ac6d
Miconen <micropizza.mc@gmail.com>
2023-03-21 18:53:33 +0200

    Remmove notify dependency

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -15,7 +15,3 @@ require("indent_blankline").setup {
     show_current_context = true,
     show_current_context_start = true,
 }
-
-require("notify").setup ({
-    background_colour = "#000000"
-})
