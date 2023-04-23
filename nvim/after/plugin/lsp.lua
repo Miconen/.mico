5defb1cfc817e9d1a522450b0d7af0634904ca45
Miconen <micropizza.mc@gmail.com>
2023-04-23 03:25:44 +0300

    Update lsp settings

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,63 +1,32 @@
-local lsp = require('lsp-zero').preset({
-    name = 'minimal',
-    set_lsp_keymaps = true,
-    manage_nvim_cmp = true,
-    suggest_lsp_servers = false,
-})
+local lsp = require('lsp-zero').preset("recommended")
 
--- (Optional) Configure lua language server for neovim
-lsp.nvim_workspace()
+local cmp = require("cmp")
+local cmp_action = require("lsp-zero").cmp_action()
 
-lsp.ensure_installed({
-    "lua_ls",
-    -- C#
-    "omnisharp",
-    -- TypeScript
-    "tsserver",
-    "angularls",
-    "astro",
-    -- HTML & CSS
-    "html",
-    "cssls",
-    "emmet_ls",
-    -- Docker
-    "dockerls",
-    "docker_compose_language_service",
-})
-
-lsp.setup()
-
-local cmp = require('cmp')
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<s-TAB>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<TAB>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete(),
-})
--- local cmp_formatting = lsp.defaults.cmp_formatting({
---     format = require('lspkind').cmp_format({
---         mode = "symbol"
---     })
--- })
-
-lsp.set_preferences({
-    -- sign_icons = {}
+cmp.setup({
+    mapping = {
+        ['<TAB>'] = cmp_action.luasnip_supertab(cmp_select),
+        ['<S-TAB>'] = cmp_action.luasnip_shift_supertab(cmp_select),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+    },
 })
 
 lsp.on_attach(function(client, bufnr)
-    local opts = { buffer = bufnr, remap = false }
-    -- utility
-    vim.keymap.set("n", "<leader>.", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    local opts = { buffer = bufnr, remap = false },
 
-    -- errors
+    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>.", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+    vim.keymap.set("n", "<F2>", function() vim.lsp.buf.rename() end, opts)
+
+    -- Diagnostics
     vim.keymap.set("n", "ge", function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set("n", "gE", function() vim.diagnostic.goto_prev() end, opts)
 
-    -- format
-    vim.keymap.set("n", "<leader>l", function() vim.lsp.buf.format() end, opts)
+    -- Format buffer
+    vim.keymap.set({'n', 'x'}, '<leader>l', function() vim.lsp.buf.format({async = false, timeout_ms = 3000}) end, opts)
 end)
+
+lsp.setup()
