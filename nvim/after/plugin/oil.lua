d352392012cfb4f831cb524761c0a91ae4c84ea5
Miconen <micropizza.mc@gmail.com>
2023-05-24 15:10:09 +0300

    Add extra exit keybind to oil.nvim popup window

--- nvim/after/plugin/oil.lua
+++ nvim/after/plugin/oil.lua
@@ -1,3 +1,11 @@
 require("oil").setup({})
 
+-- Open oil in a floating window
 vim.keymap.set("n", "-", require("oil").open_float, { desc = "Open parent directory with oil.nvim" })
+
+-- Add <Esc> as one of the exit keys to oil
+vim.keymap.set("n", "<Esc>", function()
+  if vim.fn.win_gettype(vim.fn.winnr()) == "popup" then
+    require("oil").close()
+  end
+end, { desc = "Close oli.nvim filetree buffer" })
