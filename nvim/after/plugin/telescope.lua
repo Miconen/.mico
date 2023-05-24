19af6e076c037e8449f64b693633b7d867524cc6
Miconen <micropizza.mc@gmail.com>
2023-05-24 14:33:16 +0300

    Remove telescope file browser, deprecated by oil.nvim

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -22,6 +22,5 @@ require("telescope").load_extension("file_browser")
 
 local opts = { noremap = true }
 vim.api.nvim_set_keymap("n", "<C-P>", "<cmd>lua require('telescope.builtin').find_files()<CR>", opts)
-vim.api.nvim_set_keymap("n", "<C-N>", ":Telescope file_browser<CR>", opts)
 vim.api.nvim_set_keymap("n", "<C-F>", "<cmd>lua require('telescope.builtin').live_grep()<CR>", opts)
 vim.api.nvim_set_keymap("n", "<C-B>", "<cmd>lua require('telescope.builtin').oldfiles()<CR>", opts)
