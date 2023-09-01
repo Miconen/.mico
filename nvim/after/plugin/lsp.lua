acc63c19a3b7197cc3fa8ad9d82e0e393f38e83b
Miconen <micropizza.mc@gmail.com>
2023-09-02 01:53:03 +0300

    nvim cmp formatting

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,17 +1,5 @@
 local lsp = require("lsp-zero").preset("recommended")
 
-local cmp = require("cmp")
-local cmp_action = require("lsp-zero").cmp_action()
-
-cmp.setup({
-	mapping = {
-		["<TAB>"] = cmp_action.luasnip_supertab(cmp_select),
-		["<S-TAB>"] = cmp_action.luasnip_shift_supertab(cmp_select),
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
-		["<C-Space>"] = cmp.mapping.complete(),
-	},
-})
-
 lsp.on_attach(function(_, bufnr)
 	local opts =
 		{ buffer = bufnr, remap = false }, vim.keymap.set("n", "K", function()
@@ -45,3 +33,50 @@ lsp.on_attach(function(_, bufnr)
 end)
 
 lsp.setup()
+
+local cmp = require("cmp")
+local compare = require("cmp.config.compare")
+local cmp_action = require("lsp-zero").cmp_action()
+
+local autocomplete_group = vim.api.nvim_create_augroup("vimrc_autocompletion", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "sql", "mysql", "plsql" },
+	callback = function()
+		cmp.setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
+	end,
+	group = autocomplete_group,
+})
+
+cmp.setup({
+	mapping = {
+		["<TAB>"] = cmp_action.luasnip_supertab(cmp_select),
+		["<S-TAB>"] = cmp_action.luasnip_shift_supertab(cmp_select),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<C-Space>"] = cmp.mapping.complete(),
+	},
+	formatting = {
+		fields = { "kind", "abbr", "menu" },
+		format = function(entry, vim_item)
+			local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+			local strings = vim.split(kind.kind, "%s", { trimempty = true })
+			kind.kind = " " .. (strings[1] or "") .. " "
+			kind.menu = "    (" .. (strings[2] or "") .. ")"
+
+			return kind
+		end,
+	},
+	sorting = {
+		comparators = {
+			compare.exact,
+			compare.recently_used,
+		},
+	},
+	window = {
+		completion = cmp.config.window.bordered({
+            border = "none",
+			winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+			col_offset = -3,
+			side_padding = 0,
+		}),
+	},
+})
